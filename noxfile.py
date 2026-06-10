d18f03f163c32bb465b7d2a10a43ea0149ced28b
Timothy H. Click <thclick@umary.edu>
2026-06-10 10:43:36 -0500

    doc: updated copyright information in comments
    Updated the comment header to include a statement about each file being
    included in the project. Removed the last paragraph and added separate
    first and last lines of -.
    
    Signed-off-by: Timothy H. Click <thclick@umary.edu>

--- noxfile.py
+++ noxfile.py
@@ -1,23 +1,21 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# fluctmatch
-# Copyright (c) 2026 Timothy H. Click
+# ------------------------------------------------------------------------------
+#  Copyright (C) 2026 Timothy H. Click
 #
-# This file is part of fluctmatch.
+#  This file is part of mdscaffold.
 #
-# Fluctmatch is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
-# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-# Fluctmatch is distributed in the hope that it will be useful, # but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with this program.
-# If not, see <[1](https://www.gnu.org/licenses/)>.
-#
-# Reference:
-# Timothy H. Click, Nixon Raj, and Jhih-Wei Chu. Simulation. Meth Enzymology. 578 (2016), 327-342,
-# Calculation of Enzyme Fluctuograms from All-Atom Molecular Dynamics doi:10.1016/bs.mie.2016.05.024.
-# ---------------------------------------------------------------------------------------------------------------------
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ------------------------------------------------------------------------------
 """Nox configuration file for automating development tasks for the `fluctmatch` project.
 
 This script defines sessions for:
