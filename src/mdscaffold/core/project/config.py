334e17db7e3b82b5ae8002dfd4fa9b3b31070f5a
Timothy H. Click <thclick@umary.edu>
2026-06-11 10:45:49 -0500

    refactor: move config.py to project subpackage.
    Move configuration from core package to project subpackage for clarity.

--- src/mdscaffold/core/project/config.py
+++ src/mdscaffold/core/project/config.py
@@ -1,5 +1,22 @@
 # ------------------------------------------------------------------------------
-#  Copyright (C) 2026 Timothy H. Click
+#  Copyright (c) 2026 Timothy H. Click, Ph.D.
+#
+#  This file is part of mdscaffold.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+# ------------------------------------------------------------------------------
 #
 #  This file is part of mdscaffold.
 #
@@ -460,4 +477,4 @@ class ProjectConfig(BaseModel):
 
         self.root.mkdir(parents=True, exist_ok=True)
         with open(self.toml_path, "wb") as f:
-            tomli_w.dump(self.model_dump(mode="json"), f)
+            tomli_w.dump(self.model_dump(mode="json", exclude_none=True), f)
